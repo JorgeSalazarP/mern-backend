1b69a6b5fc7216cb86916259034691e485bb7cb1
Jorge Salazar <j-salazarp@hotmail.com>
2021-10-16 21:36:21 +0200

    Create delete category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -13,13 +13,30 @@ exports.create = async (req,res,next) =>{
 
 }
 
-exports.list = async(req,res,next)=>{
+exports.list = async (req,res,next) =>{
 
     try {
         const result = await Category.find();
         res.json(result);
         
-    } catch (error) {
+    } catch (err) {
+        next(err);
+    }
+
+}
+
+
+exports.remove = async (req,res,next) =>{
+    try {
+        const _id = req.params.id;
+        const categoryById = await Category.findOne({ _id: _id });
+        if(!categoryById){
+            return res.status(404).json({ error: 'not found'});
+        }
+        const deleteCategory = await Category.remove(categoryById);
+        res.json({ message: `The Category ${deleteCategory} has been deleted successfully` });
+       
+    } catch (err) {
         next(err);
     }
 
--- routes/category.js
+++ routes/category.js
@@ -3,9 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, list } = require('../controllers/categoryController');
+const { create, list, remove } = require('../controllers/categoryController');
 
 router.get('/categories',list);
+router.delete('/:id',remove);
 router.post('/create',create);
 
 
